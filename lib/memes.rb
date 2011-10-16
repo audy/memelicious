2da7ccdfb794ac76bc9a40e91a0c467b979946ff
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-10-16 00:46:06 -0400

    yo dawg

--- lib/memes.rb
+++ lib/memes.rb
@@ -8,5 +8,10 @@ class Memes
   
     # "North Korea is best Korea"
     :is_best => /(\w*\b) (\w*\b) is best (\w*\b)/
+    
+    # Yo dawg I heard you like regexp so I put a regexp in your regexp so you can blah
+    :yo_dawg => /yo dawg I hea{,0}rd you like (.*) so I put a (.*) in your (.*) so you can (.*) while you (.*)/
+    
+    
   }
 end
