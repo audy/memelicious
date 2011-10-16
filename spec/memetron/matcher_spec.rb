c4e7f974ca5506857e29cc1bc836e8fb3683e044
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-10-16 17:08:26 -0400

    wrote tests for parsing

--- spec/memetron/matcher_spec.rb
+++ spec/memetron/matcher_spec.rb
@@ -13,6 +13,10 @@ describe Memetron::Matcher, "#match" do
     it "requires a subject" do
       subject.match("a wild  appears").should be_nil
     end
+
+    it "gets variables" do
+      subject.parse(:pokemon, "a wild charizard appears").should == ['charizard']
+    end
   end
 
   context "Dos Equis" do
@@ -23,11 +27,19 @@ describe Memetron::Matcher, "#match" do
     it "is detected despite poor grammar" do
       subject.match("I dont always go crazy but when I do I go all the way").should == :dos_equis
     end
+
+    it "gets variables" do
+      subject.parse(:dos_equis, "I don't always x but when I do y").should == ['x', 'y']
+    end
   end
 
   context "Is best" do
     it "is detected" do
       subject.match("north korea is best korea").should == :is_best
+    end
+
+    it "gets variables" do
+      subject.parse(:is_best, "north korea is best korea").should == ["north", "korea", "korea"]
     end
   end
 
@@ -39,6 +51,10 @@ describe Memetron::Matcher, "#match" do
     it "is detected despite poor grammar" do
       subject.match("yo dawg i herd you like stereos so i put a stereo in your car so you can listen while you listen").should == :yo_dawg
     end
+
+    it "gets variables" do
+      subject.parse(:yo_dawg, "yo dawg i heard you like stereos so i put a stereo in your car so you can listen while you listen").should == ['stereos', 'stereo', 'car', 'listen', 'listen']
+    end
   end
 
   context "Fry" do
@@ -49,6 +65,10 @@ describe Memetron::Matcher, "#match" do
     it 'is detected with "Not sure"' do
       subject.match("not sure if joking or not").should == :fry
     end
+
+    it "gets variables" do
+      subject.parse(:fry, "cant tell if joking or not").should == ['cant tell', 'joking', 'not']
+    end
   end
 
   context "Patrick" do
@@ -58,6 +78,10 @@ describe Memetron::Matcher, "#match" do
 
     it "is detected despite poor grammar" do
       subject.match("lets take all the languages and put them over here").should == :patrick
+    end
+
+    it "gets variables" do
+      subject.parse(:patrick, "let's take all the languages and put them over here").should == ['languages']
     end
   end
 
@@ -77,6 +101,10 @@ describe Memetron::Matcher, "#match" do
     it "is detected with an ellipsis" do
       subject.match("SOON...").should == :soon
     end
+
+    it "gets variables" do
+      subject.parse(:soon, "SOON...").should == ['...']
+    end
   end
 
   context "Y U NO" do
@@ -91,17 +119,29 @@ describe Memetron::Matcher, "#match" do
     it "requires a question mark" do
       subject.match("PROGRAMMERS, Y U NO DO THIS").should be_nil
     end
+
+    it "gets variables" do
+      subject.parse(:y_u_no?, "Y U NO DO THIS?").should == ['', 'DO THIS']
+    end
   end
-  
+
   context "Hipster Kitty" do
     it "is detected" do
       subject.match("I liked Ruby before Matz was born").should == :hipster_kitty
     end
+
+    it "gets variables" do
+      subject.parse(:hipster_kitty, "I liked Ruby before Matz was born").should == ['Ruby', 'Matz was born']
+    end
   end
-  
+
   context "Bear Grillis" do
     it "is detected" do
       subject.match("Write a Ruby library. Better drink my own piss").should == :bear_grillis
+    end
+
+    it "gets variables" do
+      subject.parse(:bear_grillis, "Write a Ruby library. Better drink my own piss").should == ["Write a Ruby library."]
     end
   end
 end
