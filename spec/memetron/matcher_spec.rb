6ffbf71466bdb49403e5d3d61395802ec4fe2388
Austin G. Davis-Richardson <harekrishna@gmail.com>
2012-09-30 16:02:38 -0400

    new matchers specs

--- spec/memetron/matcher_spec.rb
+++ spec/memetron/matcher_spec.rb
@@ -4,4 +4,12 @@ describe Memetron::Matcher do
   it 'should match a meme' do
     Memetron::Matcher.match('a wild test case appears').should == [Memetron::Pokemon, 'test case']
   end
+
+  it 'has a method [] which also matches a meme' do
+    Memetron::Matcher['a wild test case appears'].should == [Memetron::Pokemon, 'test case']
+  end
+
+  it 'should return nil for a non-existant meme' do
+    Memetron::Matcher['1234'].should be_nil
+  end
 end
