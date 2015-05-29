8471806276e6ec6f0b8d6be16470dfab409bb9ed
Austin Davis-Richardson <harekrishna@gmail.com>
2015-05-28 23:46:00 -0400

    updated rspec syntax for #4

--- spec/memetron/matcher_spec.rb
+++ spec/memetron/matcher_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Memetron::Matcher do
   it 'should match a meme' do
-    Memetron::Matcher.match('a wild test case appears').should == [Memetron::Pokemon, 'test case']
+    expect(Memetron::Matcher.match('a wild test case appears')).to eq([Memetron::Pokemon, 'test case'])
   end
 
   it 'has a method [] which also matches a meme' do
-    Memetron::Matcher['a wild test case appears'].should == [Memetron::Pokemon, 'test case']
+    expect(Memetron::Matcher['a wild test case appears']).to eq ([Memetron::Pokemon, 'test case'])
   end
 
   it 'should return nil for a non-existant meme' do
-    Memetron::Matcher['1234'].should be_nil
+    expect(Memetron::Matcher['1234']).to eq(nil)
   end
 end
